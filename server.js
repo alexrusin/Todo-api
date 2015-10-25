c51c64228e35aa7aefc2552bf8e033319ecbf766
Alex Rusin <alexrusin_2000@yahoo.com>
2015-10-25 13:18:17 -0700

    Convert get todos to sequlize

--- server.js
+++ server.js
@@ -16,26 +16,47 @@ app.get('/', function(req, res) {
 
 //GET todos?completed=false&q=work
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where={};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(todos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(todos, {
-			completed: false
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed=true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed=false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.toLowerCase().q) > -1;
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description={
+			$like: '%'+query.q+'%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+
+	}, function(e){
+		res.status(500).send;
+	});
+
+	// var filteredTodos = todos;
+
+	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	// 	filteredTodos = _.where(todos, {
+	// 		completed: true
+	// 	});
+	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	// 	filteredTodos = _.where(todos, {
+	// 		completed: false
+	// 	});
+	// }
+
+	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
+	// 		return todo.description.toLowerCase().indexOf(queryParams.toLowerCase().q) > -1;
+	// 	});
+	// }
+
+	// res.json(filteredTodos);
 });
 
 //GET todos/:id
@@ -50,7 +71,7 @@ app.get('/todos/:id', function(req, res) {
 			res.status(404).send();
 		} 
 	}, function(e){
-		res.status(505).send();
+		res.status(500).send();
 	});	
 });
 		
