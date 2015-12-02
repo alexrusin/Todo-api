83078814a7232300d02944e36ebda07cd7443ba0
Alex Rusin <alexrusin_2000@yahoo.com>
2015-12-02 12:01:06 -0800

    Adding Delete /users/login

--- server.js
+++ server.js
@@ -53,9 +53,8 @@ app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		where: {
 			id: todoId,
 			userId: req.user.get('id')
-
 		}
-		}).then(function(todo) {
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -150,17 +149,28 @@ app.post('/users', function (req, res) {
 // POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());	
-		} else {
-			res.status(401).send();
-		}
-	}, function () {
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
+	});
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
 	});
 });
 
